e437e0d85fbd89ffdf17b79ed53afc4a6e273221
dav-sap <davidsaper@gmail.com>
2023-07-11 18:39:06 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -19,45 +18,5 @@ module.exports = {
   plugins: [
     "@typescript-eslint",
     "react-hooks"
-  ]  
-  extends: [
-    'airbnb',
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended'
-  ],
-  plugins: [
-    'react-hooks',
-  ],
-  settings: {
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    },
-  },
-  rules: {
-    'import/prefer-default-export': 'off',
-    'no-plusplus': 'off',
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        "": "never",
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-   ],
-   "no-console": ["error", { allow: ["error"] }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
-   'react/function-component-definition': "off",
-   "react/prop-types": "off", // Since we do not use prop-types
-   "react/require-default-props": "off", // Since we do not use prop-types
-   "react/jsx-filename-extension": "off"
-  },
-  
+  ]
 };
