e11cc0ad290e9af61a06a575c00056761d841837
Nir Parisian <nir.parisian@gmail.com>
2023-08-06 10:33:20 +0300

    import config

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:storybook/recommended",
         "prettier",
+        "plugin:react/jsx-runtime"
       ],
       rules: {
         "no-console": ["warn", { allow: ["warn"] }], // please don't make an error occur here we use console.error
@@ -15,12 +16,20 @@ module.exports = {
         "no-debugger": "error",
         "@typescript-eslint/ban-ts-comment": "off",
         "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "error"
+        "react-hooks/exhaustive-deps": "error",
+        "react/prop-types": "off",
       },
       parser: "@typescript-eslint/parser",
       plugins: [
         "@typescript-eslint",
         "react-hooks",
         "jsx-a11y"
-      ]
+      ],
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+      },
 };
