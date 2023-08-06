d735400df556698e3e1ad68111cbba141bca050f
Nir Parisian <nir.parisian@gmail.com>
2023-08-06 19:54:38 +0300

    we need to avoid extending since it causes loops

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:react-hooks/recommended",
         "plugin:import/recommended",
-        "plugin:@typescript-eslint/recommended",
         "plugin:storybook/recommended",
         "prettier",
         "plugin:react/jsx-runtime"
